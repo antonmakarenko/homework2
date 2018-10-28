097d0caf2d7389c4f3cd2c84d34d0e05e1c2b21a
Anton Makarenko <anton@makarenko.com>
2018-10-28 18:53:18 -0500

    Break down into 3 components: menu, weather, social

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,16 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { MenuComponent } from './widget/menu/menu.component';
+import { WeatherComponent } from './widget/weather/weather.component';
+import { SocialComponent } from './widget/social/social.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MenuComponent,
+    WeatherComponent,
+    SocialComponent
   ],
   imports: [
     BrowserModule
